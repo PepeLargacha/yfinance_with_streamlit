1a814d25e02d07f0794b409ea659b092fa99916c
Pedro Largacha <86388846+PepeLargacha@users.noreply.github.com>
2022-06-15 15:41:31 -0300

    Create main.py
    Create the initial app

---
+++ main.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Showing closing price and volume for Google
+""")
+
+tickersymbol = 'GOOGL'
+tickerdata = yt.Ticker(tickersymbol)
+ticker_df = tickerdata.history(period='1d', start='2010-5-31', end='2022-6-14')
+
+st.line_chart(ticker_df.Close)
+st.line_chart(ticker_df.Volume)
